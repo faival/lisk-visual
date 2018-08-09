9245e8b4874eb0005feffd04319ca14ecc87b7a7
Pablo Molina <pablo@lightcurve.io>
2018-08-09 17:38:36 +0200

    show date of blocks

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,32 +4,37 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import {
-	requestPeers,
-	initSockets,
+    requestPeers,
+    initSockets,
 } from '../../store/reducers/peers'
 
 class Home extends React.Component {
 
-	componentDidMount() {
-		this.props.initSockets();
-		this.props.requestPeers();
-	}
-	render () {
+    componentDidMount() {
+        this.props.initSockets();
+        this.props.requestPeers();
+    }
+    render () {
 
-		const props = this.props;
-		const peers = props.nodes.map(node => (<p key={node.nonce}>{node.ip}</p>));
-
-		return (
-		  <div>
-		    {peers}
-		  </div>
-		);
-	}
+        const { blocks } = this.props;
+        // const peers = props.nodes.map(node => (<p key={node.nonce}>{node.ip}</p>));
+        const renderedBlocks = blocks.map(block => (
+            <div key={block.id}>
+                <h1>{new Date(block.timestamp).toString()}</h1>
+            </div>));
+        return (
+          <div>
+                {/* {peers} */}
+                {renderedBlocks}
+          </div>
+        );
+    }
 }
   
 
 const mapStateToProps = ({ counter, peers}) => ({
   nodes: peers.nodes,
+  blocks: peers.blocks,
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
