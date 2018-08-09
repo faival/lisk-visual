2a8cda87ac6faab04bdea180914724fb66319cfe
michaeltomasik <shugo12345@gmail.com>
2018-08-09 17:59:38 +0200

    Added moment and heatmap

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import HeatMap from 'react-heatmap-grid';
+import moment from 'moment';
 
 import {
     requestPeers,
@@ -10,47 +12,35 @@ import {
 
 class Home extends React.Component {
 
-<<<<<<< HEAD
-	componentDidMount() {
-		this.props.initSockets();
-		this.props.requestPeers();
-	}
-	render () {
-
-		const { blocks } = this.props;
-		// const peers = props.nodes.map(node => (<p key={node.nonce}>{node.ip}</p>));
-		const renderedBlocks = blocks.map(block => (
-			<div key={block.id}>
-				<h1>{new Date(block.timestamp)}</h1>
-			</div>));
-		return (
-		  <div>
-				{/* {peers} */}
-				{renderedBlocks}
-		  </div>
-		);
-	}
-=======
     componentDidMount() {
         this.props.initSockets();
         this.props.requestPeers();
     }
     render () {
-
+			const charBlocksDate = this.props.blocks.map(block => moment(new Date(block.timestamp)).format('MMMM Do YYYY, h:mm:ss a'))
+			const xLabels = charBlocksDate.map(date => `${date}`);
+			const yLabels = ['Blocks'];
+			const data = new Array(yLabels.length)
+				.fill(0)
+				.map(() => new Array(xLabels.length).fill(0).map(() => Math.floor(Math.random() * 100)));
+ 
         const { blocks } = this.props;
         // const peers = props.nodes.map(node => (<p key={node.nonce}>{node.ip}</p>));
         const renderedBlocks = blocks.map(block => (
             <div key={block.id}>
-                <h1>{new Date(block.timestamp).toString()}</h1>
+                <h1>{moment(new Date(block.timestamp)).format('MMMM Do YYYY, h:mm:ss a')}</h1>
             </div>));
         return (
           <div>
-                {/* {peers} */}
+						<HeatMap
+							xLabels={xLabels}
+							yLabels={yLabels}
+							data={data}
+						/>
                 {renderedBlocks}
           </div>
         );
     }
->>>>>>> 9245e8b4874eb0005feffd04319ca14ecc87b7a7
 }
   
 
